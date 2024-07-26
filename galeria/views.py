5be60c1d90fb2281a85fdb6c14f12f88d46d7d08
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2024-07-26 17:30:24 -0300

    utilizando FOR para informações dinâmicas

--- galeria/views.py
+++ galeria/views.py
@@ -3,9 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+dados = {
+  1: {'nome': 'Nebulosa de Carina', 'legenda': 'webbtelescope.org / NASA / James Webb'},
+  2: {'nome': 'Galáxia NGC 1079', 'legenda': 'nasa.org / NASA / Hubble'},
+}
+
 def index(request):
   # return HttpResponse('<h1>Alura Space</h1><p>Bem-vindo ao espaço Alura!</p>')
-  return render(request, 'galeria/index.html')
+  return render(request, 'galeria/index.html', {'cards': dados})
 
 
 def imagem(request):
