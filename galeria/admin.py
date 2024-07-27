702314c72a0e566555a141fead9a3eaa1c00b381
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2024-07-27 16:15:52 -0300

    adicionando categoria no model Fotografia e criando/aplicando sua migration

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from galeria.models import Fotografia
+
+class ListandoFotografias(admin.ModelAdmin):
+  list_display = ('id', 'nome', 'legenda')
+  list_display_links = ('id', 'nome')
+  search_fields = ('nome',)
 
 # Register your models here.
+
+admin.site.register(Fotografia, ListandoFotografias)
