dd1d59a49fa70f76df9a09c10456dd17fa3915d1
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2024-07-28 18:30:09 -0300

    configurando formulario de login e cadastro e criando usuario e persistindo no banco

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from usuarios import forms
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -12,4 +13,27 @@ def login(request):
 def cadastro(request):
   form = forms.CadastroForms()
   pass_fields = ['password_1', 'password_2']
+
+  if request.method == 'POST':
+    form = forms.CadastroForms(request.POST)
+    
+    if form.is_valid():
+      if form['password_1'].value() != form['password_2'].value():
+        return redirect(to='cadastro')
+      
+      nome = form['nome_cadastro'].value()
+      email = form['email'].value()
+      senha = form['password_1'].value()
+
+      if User.objects.filter(username=nome).exists() or User.objects.filter(email=email).exists():
+        return redirect(to='cadastro')
+      
+      usuario = User.objects.create_user(
+        username=nome,
+        email=email,
+        password=senha,
+      )
+      usuario.save()
+      return redirect(to='login')
+
   return render(request, 'usuarios/cadastro.html', {'form': form, 'pass_fields': pass_fields})
