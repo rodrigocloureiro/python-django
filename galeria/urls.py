f2bddfa89e273f87c2656efe1c5f09a81532ad21
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2024-07-27 21:12:33 -0300

    implementando busca na pagina

--- galeria/urls.py
+++ galeria/urls.py
@@ -4,4 +4,5 @@ from galeria import views
 urlpatterns = [
   path('', views.index, name='index'),
   path('imagem/<int:foto_id>', views.imagem, name='imagem'),
+  path('buscar', views.buscar, name='buscar'),
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -14,3 +14,14 @@ def index(request):
 def imagem(request, foto_id):
   fotografia = get_object_or_404(Fotografia, pk=foto_id)
   return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
+
+
+def buscar(request):
+  fotografias = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
+
+  if 'busca' in request.GET:
+    nome = request.GET['busca']
+    if nome:
+      fotografias = fotografias.filter(nome__icontains=nome)
+
+  return render(request, 'galeria/buscar.html', {'cards': fotografias})
